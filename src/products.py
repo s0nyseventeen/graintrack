f3ca9004567bd84783dfe69180fe5c5eabafab17
Stanislav Kryshchuk <503282404@gehealthcare.com>
2024-10-13 01:02:50 +0300

    Implement an endpoint in order to change product's price

--- src/products.py
+++ src/products.py
@@ -27,3 +27,15 @@ def create_product():
 def get_all():
     products = Product.query.all()
     return jsonify([product.to_dict() for product in products])
+
+
+@bp.route('/<int:product_id>', methods=['PATCH'])
+def update_price(product_id):
+    data = request.get_json()
+    if not data:
+        abort(400, description='Required field: "price"')
+        
+    product = Product.query.get_or_404(product_id)
+    product.price = data['price']
+    db.session.commit()
+    return jsonify({'message': 'Price was changed successfully'}), 204
--- tests/test_products.py
+++ tests/test_products.py
@@ -53,3 +53,13 @@ def test_get_all(client, create_prod):
     assert all(
         prod['name'] == f'Product{i+1}' for i, prod in enumerate(products)
     )
+
+
+def test_update_price(app, client, create_prod):
+    prod_id = create_prod('Product1', 20.0, 1)
+    resp = client.patch(f'/products/{prod_id}', json={'price': 25.0})
+    assert resp.status_code == 204
+    
+    with app.app_context():
+        updated_prod = db.session.get(Product, prod_id)
+        assert updated_prod.price == 25.0
